ccb862b7119135518063dbd4e972643cc157933e
Cgun-art <migueloproio903@gmail.com>
2025-04-14 11:24:05 -0300

    Create rtx.bash

---
+++ rtx.bash
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Loop through all files in the current directory
+for file in *; do
+  if [[ -f "$file" ]]; then
+    echo "Processing file: $gtx.cpp"
+    # Add your processing commands here
+  fi
+done
